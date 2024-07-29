b8fb683ee88e89579762a194adb3c0b13978e9c7
anyars-encarta <anyarsencarta@gmail.com>
2024-07-29 17:40:16 +0000

    Animate mobile menu

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -13,9 +13,9 @@ const MobileMenu = () => {
             <div className='flex flex-col gap-[4.5px]'
                 onClick={() => setIsOpen((prev) => !prev)}
             >
-                <div className='w-6 h-1 bg-blue-500 rounded-sm' />
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen && 'hidden'}`} />
-                <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? 'rotate-45' : ''} origin-left ease-in-out duration-500`} />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? 'opacity-0' : ''}`} />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? '-rotate-45' : ''} origin-left ease-in-out duration-500`} />
             </div>
 
             {isOpen && (
