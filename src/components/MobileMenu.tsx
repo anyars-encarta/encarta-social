020d557732f5c1f1947a05c7a7638676d554920f
anyars-encarta <anyarsencarta@gmail.com>
2024-07-29 16:20:05 +0000

    Create a Mobile Menu

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MobileMenu = () => {
+  return (
+    <div className=''>
+        <div className='flex flex-col gap-[4.5px]'>
+            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+        </div>
+    </div>
+  )
+}
+
+export default MobileMenu
