a25a222bd8635f86e7b3930ceddaed2e543b30d1
anyars-encarta <anyarsencarta@gmail.com>
2024-07-29 17:30:58 +0000

    Add a Mobile menu items

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+'use client';
+
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 const MobileMenu = () => {
-  return (
-    <div className=''>
-        <div className='flex flex-col gap-[4.5px]'>
-            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
-            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
-            <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+    const [isOpen, setIsOpen] = useState(false);
+
+    console.log(isOpen);
+
+    return (
+        <div className=''>
+            <div className='flex flex-col gap-[4.5px]'
+                onClick={() => setIsOpen((prev) => !prev)}
+            >
+                <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen && 'hidden'}`} />
+                <div className='w-6 h-1 bg-blue-500 rounded-sm' />
+            </div>
+
+            {isOpen && (
+                <div className='absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10'>
+                    <Link href='/'>Home</Link>
+                    <Link href='/'>Friends</Link>
+                    <Link href='/'>Groups</Link>
+                    <Link href='/'>Stories</Link>
+                    <Link href='/'>Login</Link>
+                </div>
+            )}
         </div>
-    </div>
-  )
+    )
 }
 
 export default MobileMenu
