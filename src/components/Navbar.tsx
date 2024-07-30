3688aade62e058b1e9377ace4d43dfd3a61d3604
anyars-encarta <anyarsencarta@gmail.com>
2024-07-30 16:50:46 +0000

    Add Sig-in and Sign-up authentications

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
 import Image from 'next/image'
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
     return (
@@ -32,6 +33,32 @@ const Navbar = () => {
 
             {/* RIGHT */}
             <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
+                <ClerkLoading>
+                    <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                    <SignedIn>
+                        <div className='cursor-pointer'>
+                            <Image src='/people.png' alt='' width={20} height={20} />
+                        </div>
+                        <div className='cursor-pointer'>
+                            <Image src='/messages.png' alt='' width={20} height={20} />
+                        </div>
+                        <div className='cursor-pointer'>
+                            <Image src='/notifications.png' alt='' width={20} height={20} />
+                        </div>
+                        <UserButton />
+                    </SignedIn>
+
+                    <SignedOut>
+                        <div className='flex items-center justify-center gap-2 text-sm'>
+                            <Image src='/login.png' alt='' width={20} height={20} />
+                            <Link href='/sign-in'>Login/Register</Link>
+                        </div>
+                        {/* <SignInButton /> */}
+                    </SignedOut>
+                </ClerkLoaded>
                 <MobileMenu />
             </div>
         </div>
