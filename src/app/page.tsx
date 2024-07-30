c9fa9a5d013036682832ef0da0b8b45250dfa497
anyars-encarta <anyarsencarta@gmail.com>
2024-07-30 17:22:46 +0000

    Create Left, Right and Stries components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import Stories from "@/components/Stories"
+import LeftMenu from "@/components/LeftMenu"
+import RightMenu from "@/components/RightMenu"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className='flex gap-6'>
+      {/* LEFT */}
+      <div className='hidden xl:block w-[20%]'>
+        <LeftMenu />
+      </div>
+
+      {/* CENTER */}
+      <div className='w-full lg:w-[70%] xl:w-[50%]'>
+        <Stories />
+      </div>
+
+      {/* RIGHT */}
+      <div className='hidden lg:block w-[30%]'>
+        <RightMenu />
+      </div>
+    </div>
   )
 }
 
