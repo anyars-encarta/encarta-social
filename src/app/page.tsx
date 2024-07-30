c9d285de0469ba99c56141e2259b247d90ca1c80
anyars-encarta <anyarsencarta@gmail.com>
2024-07-30 17:27:11 +0000

    Add Stories, Post and Feed components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Stories from "@/components/Stories"
 import LeftMenu from "@/components/LeftMenu"
 import RightMenu from "@/components/RightMenu"
+import AddPost from "@/components/AddPost"
+import Feed from "@/components/Feed"
 
 const Homepage = () => {
   return (
-    <div className='flex gap-6'>
+    <div className='flex gap-6 pt-6'>
       {/* LEFT */}
       <div className='hidden xl:block w-[20%]'>
         <LeftMenu />
@@ -12,7 +14,11 @@ const Homepage = () => {
 
       {/* CENTER */}
       <div className='w-full lg:w-[70%] xl:w-[50%]'>
-        <Stories />
+        <div className='flex flex-col gap-6'>
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
       </div>
 
       {/* RIGHT */}
