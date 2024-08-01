aac194969b6ab092cf800c23553fddecd0ed1a27
anyars-encarta <anyarsencarta@gmail.com>
2024-08-01 16:54:17 +0000

    Complete User Media UI design

--- src/components/UserMedia.tsx
+++ src/components/UserMedia.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,6 +10,39 @@ const UserMedia = ({ userId }: { userId: string }) => {
                 <Link href='/' className='text-blue-500 text-xs'>See all</Link>
             </div>
 
+            <div className='flex gap-4 flex-wrap justify-between'>
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+
+                <div className='relative w-1/5 h-24'>
+                    <Image src='https://images.pexels.com/photos/22027141/pexels-photo-22027141/free-photo-of-farmer-in-hat-sitting-on-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' fill alt='' className='object-cover rounded-md' />
+                </div>
+            </div>
         </div>
     )
 }
