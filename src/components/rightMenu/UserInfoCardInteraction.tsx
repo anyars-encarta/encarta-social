736f085c179f9114519cf04e6fed125804f24ed9
anyars-encarta <anyarsencarta@gmail.com>
2024-08-03 18:32:45 +0000

    Add useOptimisticHook to immediate update of the state

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { switchFollow } from "@/lib/actions";
-import { useState } from "react";
+import { useOptimistic, useState } from "react";
 
 const UserInfoCardInteraction = (
     {
@@ -10,41 +10,45 @@ const UserInfoCardInteraction = (
         isUserBlocked: boolean, isFollowingSent: boolean, isFollowing: boolean, userId: string, currentUserId: string
     }) => {
 
-        const [userState, setUserState] = useState({
-            following: isFollowing,
-            blocked: isUserBlocked,
-            followingRequestSent: isFollowingSent,
-        });
-        
-        const [loading, setLoading] = useState(false);
+    const [userState, setUserState] = useState({
+        following: isFollowing,
+        blocked: isUserBlocked,
+        followingRequestSent: isFollowingSent,
+    });
 
-        const follow = async () => {
-            setLoading(true);
-            try {
-                await switchFollow(userId);
-                setUserState((prev) => ({
-                    ...prev, 
-                    following: prev.following && false,
-                    followingRequestSent: !prev.following && !prev.followingRequestSent ? true : false,
-                }))
+    const follow = async () => {
+        switchOptimisticFollow('');
 
-                setLoading(false);
-            } catch (e) {
-                console.log(e)
-                setLoading(false);
-            }
+        try {
+            await switchFollow(userId);
+            setUserState((prev) => ({
+                ...prev,
+                following: prev.following && false,
+                followingRequestSent: !prev.following && !prev.followingRequestSent ? true : false,
+            }))
+        } catch (e) {
+            console.log(e)
         }
+    };
+
+    const [optimisticFollow, switchOptimisticFollow] = useOptimistic(
+        userState, (state) => ({
+            ...state,
+            following: state.following && false,
+            followingRequestSent: !state.following && !state.followingRequestSent ? true : false,
+        })
+    )
 
     return (
         <>
             <form action={follow}>
                 <button className='w-full bg-blue-500 text-white text-sm p-2 rounded-md'>
-                    {userState.followingRequestSent ? 'Friend Request Sent' : userState.following ? 'Following' : 'Follow'}
+                    {optimisticFollow.followingRequestSent ? 'Friend Request Sent' : optimisticFollow.following ? 'Following' : 'Follow'}
                 </button>
             </form>
 
             <form action='' className='self-end'>
-                <span className='text-red-400 cursor-pointer text-xs'>{userState.blocked ? 'Unblock User' : 'Block User'}</span>
+                <span className='text-red-400 cursor-pointer text-xs'>{optimisticFollow.blocked ? 'Unblock User' : 'Block User'}</span>
             </form>
         </>
     )
