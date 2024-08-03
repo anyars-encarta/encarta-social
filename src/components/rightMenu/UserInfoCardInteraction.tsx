3fe91c520168cabfc2670e515bb35f9adf9684bd
anyars-encarta <anyarsencarta@gmail.com>
2024-08-03 19:08:40 +0000

    Implement Block user

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { switchFollow } from "@/lib/actions";
+import { switchBlock, switchFollow } from "@/lib/actions";
 import { useOptimistic, useState } from "react";
 
 const UserInfoCardInteraction = (
@@ -17,7 +17,7 @@ const UserInfoCardInteraction = (
     });
 
     const follow = async () => {
-        switchOptimisticFollow('');
+        switchOptimisticState('follow');
 
         try {
             await switchFollow(userId);
@@ -31,24 +31,42 @@ const UserInfoCardInteraction = (
         }
     };
 
-    const [optimisticFollow, switchOptimisticFollow] = useOptimistic(
-        userState, (state) => ({
-            ...state,
-            following: state.following && false,
-            followingRequestSent: !state.following && !state.followingRequestSent ? true : false,
-        })
-    )
+    const block = async () => {
+        switchOptimisticState('block');
+
+        try {
+            await switchBlock(userId);
+            setUserState((prev) => ({
+                ...prev,
+                blocked: !prev.blocked,
+            }))
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    const [optimisticState, switchOptimisticState] = useOptimistic(
+        userState, (state, value: 'follow' | 'block') => value === 'follow'
+            ? {
+                    ...state,
+                    following: state.following && false,
+                    followingRequestSent: !state.following && !state.followingRequestSent ? true : false,
+                } : {
+                    ...state,
+                    blocked: !state.blocked,
+                }
+    );
 
     return (
         <>
             <form action={follow}>
                 <button className='w-full bg-blue-500 text-white text-sm p-2 rounded-md'>
-                    {optimisticFollow.followingRequestSent ? 'Friend Request Sent' : optimisticFollow.following ? 'Following' : 'Follow'}
+                    {optimisticState.followingRequestSent ? 'Friend Request Sent' : optimisticState.following ? 'Following' : 'Follow'}
                 </button>
             </form>
 
-            <form action='' className='self-end'>
-                <span className='text-red-400 cursor-pointer text-xs'>{optimisticFollow.blocked ? 'Unblock User' : 'Block User'}</span>
+            <form action={block} className='self-end'>
+                <button className='text-red-400 cursor-pointer text-xs'>{optimisticState.blocked ? 'Unblock User' : 'Block User'}</button>
             </form>
         </>
     )
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -52,3 +52,38 @@ export const switchFollow = async (userId: string) => {
         throw new Error('Something went wrong')
     }
 }
+
+export const switchBlock = async (userId: string) => {
+    const { userId: currentUserId } = auth();
+
+    if (!currentUserId) {
+        throw new Error('User is not authenticated')
+    };
+
+    try {
+        const existingBlock = await prisma.block.findFirst({
+            where: {
+                blockerId: currentUserId,
+                blockedId: userId
+            },
+        });
+
+        if (existingBlock) {
+            await prisma.block.delete({
+                where: {
+                    id: existingBlock.id
+                },
+            });
+        } else {
+            await prisma.block.create({
+                data: {
+                    blockerId: currentUserId,
+                    blockedId: userId,
+                },
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        throw new Error('Something went wrong')
+    }
+}
